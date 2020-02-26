63c295f052c892f8b2429e8fd67ecedfde8ff9b2
wts1245 <13408804529@163.com>
2020-02-26 13:33:58 +0800

    完成了登录功能

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -56,7 +56,15 @@ export default {
     // 登录
     login () {
       this.$refs.loginFormRef.validate(async valid => {
-        console.log(valid)
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        if (res.meta.status !== 200) return this.$message.error('登录失败')
+        this.$message.success('登录成功')
+        // 1.将登录成功之后的token，保存到客户端的sessionStroage中
+        //  1.1、项目中除了登录之外的其他API接口，必须在登录之后才能访问
+        //   1.2、token 只应在当前网站打开期间生效，所以将 token 保存在 sessionStroage中
+        window.sessionStorage.setItem('token', res.data.token)
+        // 2、通过编程式导航跳转到后台主页，路由地址是 /home
+        this.$router.push('/home')
       })
     }
   }
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '@/components/Login.vue'
+import Home from '@/components/home.vue'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
@@ -12,8 +13,23 @@ const router = new VueRouter({
     {
       path: '/login',
       component: Login
+    },
+    {
+      path: '/home',
+      component: Home
     }
   ]
 })
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从哪个路径跳转而来
+  // next 是一个函数，表示放行
+  //      next()  放行    next('/login') login表示强制跳转道的路径
 
+  if (to.path === '/login') next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
 export default router
